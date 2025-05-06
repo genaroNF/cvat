7f4dc7d4cc781b673204772b99ba8f6f45ef25a8
Genaro Nadile Flaniguen <genaronadile@gmail.com>
2025-05-05 23:52:52 -0300

    Nuclio function

---
+++ serverless/pytorch/ultralytics/YOLOv8/nuclio/main.py
@@ -0,0 +1,50 @@
+
+import io
+import base64
+import json
+import torch
+from itertools import chain
+
+import cv2
+import numpy as np
+from ultralytics import YOLO
+
+# Initialize your model
+def init_context(context):
+	context.logger.info('Init context...  0%')
+	model = YOLO('best.pt')
+	context.user_data.model_handler = model
+	context.logger.info('Init context...100%')
+
+# Inference endpoint
+def handler(context, event):
+	context.logger.info('Run custom yolov8 model')
+	data = event.body
+	image_buffer = io.BytesIO(base64.b64decode(data['image']))
+	image = cv2.imdecode(np.frombuffer(image_buffer.getvalue(), np.uint8), cv2.IMREAD_COLOR)
+
+	results = context.user_data.model_handler.predict(image, conf=0.5)
+	result = results[0]
+
+	masks = []
+	for mask in getattr(result.masks, "xy", []):
+		masks.append(mask.tolist())
+
+	confs = result.boxes.conf.tolist()
+	clss = result.boxes.cls.tolist()
+	class_name = result.names
+
+	detections = []
+	threshold = 0.1
+	for mask, conf, cls in zip(masks, confs, clss):
+		label = class_name[int(cls)]
+		# must be in this format
+		detections.append({
+			'confidence': str(float(conf)),
+			'label': label,
+			'points': list(chain.from_iterable(mask)),
+			'type': "polygon",
+		})
+
+	return context.Response(body=json.dumps(detections), headers={},
+		content_type='application/json', status_code=200)
